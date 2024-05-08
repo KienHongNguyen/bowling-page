79be0e27537601183db45f96f16f006650d340e0
KienHongNguyen <hongkien66@gmail.com>
2024-05-08 18:17:08 +1000

    add new highlight function

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Function to scroll to selected section
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
@@ -9,3 +10,30 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
 });
 
+// Function to change color for navigation menu
+document.addEventListener("DOMContentLoaded", function () {
+    const options = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.5
+    };
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                document.querySelectorAll('.navLinks a').forEach(navItem => {
+                    navItem.style.color = '';
+                });
+                const activeNav = document.querySelector(`#nav-${entry.target.id.split('-')[0]}`);
+                if (activeNav) {
+                    activeNav.style.color = '#ffff00';
+                }
+            }
+        });
+    }, options);
+
+    // Observe sections
+    document.querySelectorAll('section').forEach(section => {
+        observer.observe(section);
+    });
+});
